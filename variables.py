6123c5979c88759a36916b07defea8d7f2877d95
BenasJasilionis <benas.jasilionis@gmail.com>
2023-01-27 15:56:00 +0000

    Name, age, dob user input

---
+++ variables.py
@@ -0,0 +1,47 @@
+# Variables  # Python is a dynamically packed language, meaning you dont need to state the data type
+
+# variable_name = variable_data
+
+a = 1 #int
+b = 2 #int
+c = 3.5 #float
+hi = "Hello World!" #string
+
+
+# CTRL + / to comment out all selected lines
+
+# print(hi)
+# print(a)
+
+# print(1 + 2)
+# print(a + b)
+
+# the type method - states data type of the variable
+
+# print(type(hi))
+
+# Overwriting variables
+# print(a)
+# a = 4
+# print(a)
+
+
+# Getting user input
+# print("Hi, what is your name?")
+# name = input()
+# print("hi")
+# print(name)
+
+# Make a program that takes the users name, age and dob and prints them back out
+print("Hi, what is your name?")
+name = input()
+print("hi")
+print(name)
+print("How old are you?")
+age = input()
+print("Me too")
+print("What is your date of birth?")
+dob = input()
+print(dob)
+print("Wow we are born in the same year!")
+
